6187aed0c227aedbc864d18eaef389c28fe5463a
Mike Mitchell <alchemistikk@gmail.com>
2022-01-12 16:20:50 -0500

    Add Book object constructor

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+let bookery = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+  }
+}
+
+function appendToBookery(book) {
+  bookery.push(book);
+}
+
+
