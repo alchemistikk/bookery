eb37a3a79630d6de680b2e302c63c46ae6506931
Mike Mitchell <alchemistikk@gmail.com>
2022-01-12 16:28:33 -0500

    Add function displayBookery

--- scripts/script.js
+++ scripts/script.js
@@ -14,4 +14,15 @@ function appendToBookery(book) {
   bookery.push(book);
 }
 
+function displayBookery() {
+  // Eventually display this in viewport, not console
+  for (let i = 0; i < bookery.length; i++) {
+    console.log(bookery[i])
+  }
+}
 
+let gatsby = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 129, 'yes')
+let sunAlso = new Book('The Sun Also Rises', 'Ernest Hemingway', 259, 'no')
+appendToBookery(gatsby)
+appendToBookery(sunAlso)
+displayBookery()
